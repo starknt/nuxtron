331fd4b5e6c50dd4842d68e3b11dd75aff03ab12
starknt <1431880400@qq.com>
2024-05-19 16:22:01 +0800

    refactor: use virtual module

--- packages/nuxtron/src/nuxt/module.ts
+++ packages/nuxtron/src/nuxt/module.ts
@@ -1,13 +1,15 @@
 // fix TS2742 error
 import type {} from 'nuxt/schema'
 import { isAbsolute, join } from 'node:path'
-import { createResolver, defineNuxtModule } from '@nuxt/kit'
+import { createResolver, defineNuxtModule, useLogger, useNuxt } from '@nuxt/kit'
 import type { NuxtronOptions } from './types'
 import type { RollupConfig } from './builder/types'
 import { build } from './builder/build'
+import { devTemplate } from './builder/template'
 
 let ROLLUP_CONFIG: RollupConfig
 const resolver = createResolver(import.meta.url)
+const logger = useLogger('nuxtron')
 
 export default defineNuxtModule<NuxtronOptions>({
   meta: {
@@ -23,14 +25,15 @@ export default defineNuxtModule<NuxtronOptions>({
   },
 
   hooks: {
-    // 'vite:extendConfig': (vite, { isClient }) => {
-    //   if (isClient)
-    //     return
+    'vite:extendConfig': (vite) => {
+      const nuxt = useNuxt()
+      if (nuxt.options.dev)
+        return
 
-    //   vite.ssr = {
-    //     external: Array.isArray(vite.ssr?.external) ? [...vite.ssr.external, 'electron'] : ['electron'],
-    //   }
-    // },
+      vite.ssr = {
+        external: Array.isArray(vite.ssr?.external) ? [...vite.ssr.external, 'electron'] : ['electron'],
+      }
+    },
 
     'nitro:init': (nitro) => {
       nitro.hooks.hook('rollup:before', (nitro, config) => {
@@ -40,30 +43,10 @@ export default defineNuxtModule<NuxtronOptions>({
           config.input = resolver.resolve('./runtime/nitro-dev.ts')
       })
 
-      nitro.hooks.hookOnce('rollup:before', async (nitro) => {
+      nitro.hooks.hook('compiled', async (nitro) => {
         if (nitro.options.dev) {
-          // const _nitro = {
-          //   ...nitro,
-          //   options: {
-          //     ...nitro.options,
-          //     inlineDynamicImports: false,
-          //     entry: process.env.NUXTRON_DEV_ENTRY!,
-          //   },
-          // } satisfies Nitro
-
-          // const rollupConfig = getRollupConfig(_nitro)
-          // await _build(_nitro, {
-          //   ...rollupConfig,
-          //   output: {
-          //     ...rollupConfig.output,
-          //     entryFileNames: 'dev.mjs',
-          //   },
-          //   external: Array.isArray(rollupConfig.external) ? [...rollupConfig.external, 'electron'] : ['electron'],
-          // })
-
           await build(nitro, {
             ...ROLLUP_CONFIG,
-
             input: process.env.NUXTRON_DEV_ENTRY,
             output: {
               ...ROLLUP_CONFIG.output,
@@ -71,6 +54,7 @@ export default defineNuxtModule<NuxtronOptions>({
             },
             external: Array.isArray(ROLLUP_CONFIG.external) ? [...ROLLUP_CONFIG.external, 'electron'] : ['electron'],
           })
+          logger.success('Nuxtron entry file built')
         }
       })
     },
@@ -80,6 +64,10 @@ export default defineNuxtModule<NuxtronOptions>({
         nitro.options = {
           ...nitro.options,
           inlineDynamicImports: false,
+          virtual: {
+            ...nitro.options.virtual,
+            '#internal/nuxtron': devTemplate,
+          },
         }
         return
       }
@@ -99,6 +87,10 @@ export default defineNuxtModule<NuxtronOptions>({
         externals: {
           ...nitro.options.externals,
           external: Array.isArray(nitro.options.externals.external) ? [...nitro.options.externals.external, 'electron'] : ['electron'],
+        },
+        virtual: {
+          ...nitro.options.virtual,
+          '#internal/nuxtron': 'export default {}',
         },
       }
     },
--- packages/nuxtron/src/nuxt/types.ts
+++ packages/nuxtron/src/nuxt/types.ts
@@ -1,3 +1,6 @@
 export interface NuxtronOptions {
+  /**
+   * @description electron entry file path
+   */
   entry: string
 }
